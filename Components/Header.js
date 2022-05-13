28b4081c6cc0bd042eac2f932bbe37deece10b08
Nuri Kim <64626132+nurikimchi@users.noreply.github.com>
2022-05-13 15:53:35 -0700

    Uploaded Header (navbar) component.

---
+++ Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <nav className="header">
+            <h1 className="header__text">Groundbnb</h1>
+        </nav>
+    )
+}
